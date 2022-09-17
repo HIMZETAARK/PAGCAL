9e588503d5b91820e1f8701846c62fbb8be10b3a
himlinux <himzetaark0@gmail.com>
2022-09-17 12:43:17 -0500

    Estilos

--- js/primer_algoritmoVPN.js
+++ js/primer_algoritmoVPN.js
@@ -1,28 +1,32 @@
 //******** algoritmo para verificar si es un número primo ********
 
-var entrada = parseInt(prompt("Ingresa el número: "));
-var contador = 0;
-if(entrada != 1){
+function salida(){
+
+    var entrada =  document.getElementById("numero").value;
+    var contador = 0;
+//var entrada = parseInt(prompt("Ingresa el número:")); 
+    if(entrada != 1){
 //procesando
-    for (var a=1 ; a <= entrada ; a++) {
-    var resultado= Number. isInteger(entrada / a);
+     for (var a=1 ; a <= entrada ; a++) {
+     var resultado= Number. isInteger(entrada / a);      
         if( resultado== true){
            ++contador;
         }
-    }
+     }
 //vericando 
-    if (contador<3) {
+     if (contador<3) {
 //resultado
-        alert(entrada + " Es un número Primo. ");
-        alert("PI * "+ entrada + " = " + entrada*Math.PI);
+        alert(entrada + "    ES UN NÚMERO PRIMO \n "); 
+       alert(" \n PI * "+ entrada + " = " + entrada*Math.PI);
     }else{
-        alert(entrada + " No es primo.");
+        alert(entrada + "  NO ES PRIMO");
     }
-}else{alert("1 No es un número primo.");}
-    
+}else{alert("1 NO ES PRIMO.");}  
+
+ }
 /**RESUMEN:
  * PRIMERO  Se recibe un  número para verificar.
- * SEGUNDO  El número de $entrada lo dividimos  con cada numero del 1-9 
+ * SEGUNDO  El número de $entrada lo dividimos  con cada numero del 1-9 O
  *          para saber  si la division es entera o no
  *          la variable $resultado recibirá un valor(true o false).
  * TERCERO si es  verdadero el $contador aumentará +1.
